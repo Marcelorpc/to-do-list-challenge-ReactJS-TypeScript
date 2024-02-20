e5fdfa7d0216bc595b85b78ff977669cfe11c0d7
marcelorpc <marcelorpchaves@gmail.com>
2024-02-20 17:50:28 -0300

    create basic add task system

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,37 @@ import styles from './css/App.module.css'
 import LogoImg from './assets/Logo.svg'
 import { PlusCircle, Clipboard } from 'phosphor-react'
 import { Task } from './components/Task'
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
 function App() {
   const [tasks, setTasks] = useState([
-    'Integer urna interdum massa libero auctor neque turpis turpis semper.',
-    'Integer urna interdum massa libero auctor neque turpis turpis semper.Integer urna interdum massa libero auctor neque turpis turpis semper.Integer urna interdum massa libero auctor neque turpis turpis semper.Integer urna interdum massa libero auctor neque turpis turpis semper.Integer urna interdum massa libero auctor neque turpis turpis semper.Integer urna interdum massa libero auctor neque turpis turpis semper.Integer urna interdum massa libero auctor neque turpis turpis semper.Integer urna interdum massa libero auctor neque turpis turpis semper.Integer urna interdum massa libero auctor neque turpis turpis semper.Integer urna interdum massa libero auctor neque turpis turpis semper.Integer urna interdum massa libero auctor neque turpis turpis semper.Integer urna interdum massa libero auctor neque turpis turpis semper.'
+    'Integer urna interdum massa libero auctor neque turpis turpis semper.'
   ])
+
+  const [taskContent, setTaskContent] = useState('')
+
+  function inputOnChange(event: ChangeEvent<HTMLInputElement>) {
+    setTaskContent(event.target.value)
+  }
+
+  function handleCreateNewTask(event: FormEvent){
+    event.preventDefault()  
+    setTasks([...tasks, taskContent])
+    setTaskContent('')
+  }
 
   return (
     <div className={styles.app}>
       <header>
         <img src={LogoImg} alt="Logotipo da ToDo" />
 
-        <form>
+        <form onSubmit={handleCreateNewTask}>
           <div className={styles.inputWrapper}>
             <label htmlFor="search">Adicione uma nova tarefa</label>
-            <input type="text" placeholder='Adicione uma nova tarefa'/>
+            <input id='search' onChange={inputOnChange} value={taskContent} type="text" placeholder='Adicione uma nova tarefa'/>
           </div>
 
-          <button>
+          <button type='submit'>
             Criar
             <PlusCircle size={20}/>
           </button>
@@ -43,7 +54,7 @@ function App() {
 
         <div className={styles.taskList}>
           {tasks.map((task) => {
-            return <Task content={task} />
+            return <Task key={task} content={task} />
           })}
         </div>
       </main>
