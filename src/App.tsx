c34c2c713093f45b9a35d570c944a6ae5e07fd88
marcelorpc <marcelorpchaves@gmail.com>
2024-02-20 11:46:53 -0300

    add app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import './css/App.css'
+import styles from './css/App.module.css'
+
+import LogoImg from './assets/Logo.svg'
+import { PlusCircle, Clipboard } from 'phosphor-react'
+
+//import { Task } from './components/Task' 
 
 function App() {
   return (
-    <p></p>
+    <div className={styles.app}>
+      <header>
+        <img src={LogoImg} alt="Logotipo da ToDo" />
+
+        <form>
+          <div className={styles.inputWrapper}>
+            <label htmlFor="search">Adicione uma nova tarefa</label>
+            <input type="text" placeholder='Adicione uma nova tarefa'/>
+          </div>
+
+          <button>
+            Criar
+            <PlusCircle size={20}/>
+          </button>
+        </form>
+      </header>
+
+      <main>
+        <div className={styles.taskListHeader}>
+          <strong>Tarefas criadas<span>0</span></strong>
+          <strong>Concluídas<span>0</span></strong>
+        </div>
+
+        <div className={styles.screenEmptyContent}>
+          <Clipboard />
+          <strong>Você ainda não tem tarefas cadastradas</strong>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </div>
+      </main>
+    </div>
   )
 }
 
