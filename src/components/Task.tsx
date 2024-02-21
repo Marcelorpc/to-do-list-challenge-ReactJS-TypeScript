ddf3ce80415929a43d2ae8f3c2f00dd1fa070c5b
marcelorpc <marcelorpchaves@gmail.com>
2024-02-21 09:46:21 -0300

    add checkbox validity system

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,19 +1,25 @@
 import { Trash } from 'phosphor-react'
 import styles from '../css/Task.module.css'
+import { ChangeEvent } from 'react'
 
 export interface TaksType {
   content: string,
-  deleteTask: (taskToDelete: string) => void
+  deleteTask: (taskToDelete: string) => void,
+  handleNumberOfCompletedTasks: (checked: boolean) => void
 }
-export function Task({content, deleteTask}:TaksType) {
+export function Task({content, deleteTask, handleNumberOfCompletedTasks}:TaksType) {
   function handleDeleteTask() {
     deleteTask(content)
+  }
+
+  function handleCheckboxChange(event: ChangeEvent<HTMLInputElement>){
+    handleNumberOfCompletedTasks(event.target.checked)
   }
 
   return (
     <div className={styles.task}>
       <div className={styles.inputWrapper}>
-        <input id={content} type="checkbox" />
+        <input id={content} type="checkbox" onChange={handleCheckboxChange} />
         <label htmlFor={content}>
           {content} 
         </label>
